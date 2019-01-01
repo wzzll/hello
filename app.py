453fd8ab83c2c570ea83a637b65dc118fb3f4b8b
wangzhen <1092762798@qq.com>
2019-01-01 22:36:49 +0800

    hello

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>hello Flask</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='8000')
